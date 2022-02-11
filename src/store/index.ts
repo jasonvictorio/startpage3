3185475dc59a388802436a485ede5f2abeb2824e
Jason Victorio <jgvictorio@gmail.com>
2022-02-11 15:14:16 +1300

    chore: remove unused files

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,7 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit'
-import counterReducer from './counter'
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
   },
 })
 
--- src/views/About.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import { RouteComponentProps } from '@reach/router'
-
-export default (props: RouteComponentProps) => {
-  return <h1>this is the about page</h1>
-}
