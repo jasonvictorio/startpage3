09df555aa1f42e037a5deaf6ddaf036860eb15c9
Jason Victorio <jgvictorio@gmail.com>
2022-02-15 16:32:02 +1300

    feat: dsiplay <BookmarkList> on home page

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,6 +1,46 @@
 import React from 'react'
 import { RouteComponentProps } from '@reach/router'
 
+import BookmarkList from '../components/BookmarkList'
+import { BookmarkList as BookmarkListType } from '../types'
+
 export default (props: RouteComponentProps) => {
-  return <h1>this is the home page</h1>
+  const bookmarkLists: BookmarkListType[] = [
+    {
+      title: 'first',
+      bookmarks: [
+        { title: 'Google1', url: 'https://www.google.com' },
+        { title: 'Google2', url: 'https://www.google.com' },
+        { title: 'Google3', url: 'https://www.google.com' },
+        { title: 'Google4', url: 'https://www.google.com' },
+        { title: 'Google5', url: 'https://www.google.com' },
+        { title: 'Google6', url: 'https://www.google.com' },
+        { title: 'Google7', url: 'https://www.google.com' },
+      ],
+    },
+    {
+      title: 'second',
+      bookmarks: [
+        { title: 'Google1', url: 'https://www.google.com' },
+        { title: 'Google2', url: 'https://www.google.com' },
+        { title: 'Google3', url: 'https://www.google.com' },
+        { title: 'Google4', url: 'https://www.google.com' },
+        { title: 'Google5', url: 'https://www.google.com' },
+        { title: 'Google6', url: 'https://www.google.com' },
+        { title: 'Google7', url: 'https://www.google.com' },
+      ],
+    },
+  ]
+
+  return (
+    <div>
+      {bookmarkLists.map(({ title, bookmarks }) => (
+        <BookmarkList
+          title={title}
+          bookmarks={bookmarks}
+          key={title}
+        ></BookmarkList>
+      ))}
+    </div>
+  )
 }
