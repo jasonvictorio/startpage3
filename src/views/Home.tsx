2088ff8749a79efb25816066d040becc840384c7
Jason Victorio <jgvictorio@gmail.com>
2022-02-17 01:10:13 +1300

    refactor: move out default bookmarklist

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,36 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { RouteComponentProps } from '@reach/router'
 
 import BookmarkList from '../components/BookmarkList'
+import defaultBookmarkLists from '../bookmarkLists'
 import { BookmarkList as BookmarkListType } from '../types'
 
 export default (props: RouteComponentProps) => {
-  const bookmarkLists: BookmarkListType[] = [
-    {
-      title: 'first',
-      bookmarks: [
-        { title: 'Google1', url: 'https://www.google.com' },
-        { title: 'Google2', url: 'https://www.google.com' },
-        { title: 'Google3', url: 'https://www.google.com' },
-        { title: 'Google4', url: 'https://www.google.com' },
-        { title: 'Google5', url: 'https://www.google.com' },
-        { title: 'Google6', url: 'https://www.google.com' },
-        { title: 'Google7', url: 'https://www.google.com' },
-      ],
-    },
-    {
-      title: 'second',
-      bookmarks: [
-        { title: 'Google1', url: 'https://www.google.com' },
-        { title: 'Google2', url: 'https://www.google.com' },
-        { title: 'Google3', url: 'https://www.google.com' },
-        { title: 'Google4', url: 'https://www.google.com' },
-        { title: 'Google5', url: 'https://www.google.com' },
-        { title: 'Google6', url: 'https://www.google.com' },
-        { title: 'Google7', url: 'https://www.google.com' },
-      ],
-    },
-  ]
+  const [bookmarkLists, setBookmarkLists] = useState(defaultBookmarkLists);
+
 
   return (
     <div>
